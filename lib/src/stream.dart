4e2ea94a0680150f9b34236ff3ec154a9f6e066a
Francesco Gaudenzi <fgaudo@gmail.com>
2023-11-02 17:08:58 +0100

    dev

--- lib/src/stream.dart
+++ lib/src/stream.dart
@@ -5,8 +5,8 @@ import 'package:rxdart/rxdart.dart';
 import 'io.dart';
 import 'task.dart' as T;
 
-Stream<A> fromTask<A>(T.Task<A> t) =>
-    FromCallableStream(() => t(), reusable: true);
+Stream<A> fromTask<A>(T.Task<A> task) =>
+    FromCallableStream(task, reusable: true);
 
 Stream<A> fromIO<A>(IO<A> io) => fromTask(
       T.fromIO(io),
