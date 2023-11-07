b3a53e742272d2477dc795b771af029d8da3c7ed
Francesco Gaudenzi <fgaudo@gmail.com>
2023-11-07 16:27:32 +0100

    dev

--- lib/src/extensions/reader_io/flat_map_io.dart
+++ lib/src/extensions/reader_io/flat_map_io.dart
@@ -2,7 +2,7 @@ import '../../../io.dart' as I;
 import '../../../reader_io.dart' as RI;
 
 extension FlatMapIOReaderIOExtension<ENV, A> on RI.ReaderIO<ENV, A> {
-  RI.ReaderIO<ENV, B> flatMap<B>(
+  RI.ReaderIO<ENV, B> flatMapIO<B>(
     I.IO<B> Function(A) f,
   ) =>
       RI.flatMapIO(f)(this);
