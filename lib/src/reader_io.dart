edd309f56f633fb8e6cb8c6501b6171713e10b77
Francesco Gaudenzi <fgaudo@gmail.com>
2023-11-08 04:21:08 +0100

    dev

--- lib/src/reader_io.dart
+++ lib/src/reader_io.dart
@@ -6,7 +6,7 @@ typedef ReaderIO<ENV, A> = R.Reader<ENV, I.IO<A>>;
 ReaderIO<ENV, B> Function<ENV>(ReaderIO<ENV, A>) flatMapIO<A, B>(
   I.IO<B> Function(A) f,
 ) =>
-    <ENV>(ra) => (r) => f(ra(r)());
+    <ENV>(ra) => (r) => () => f(ra(r)())();
 
 ReaderIO<ENV, B> Function(ReaderIO<ENV, A>) flatMap<ENV, A, B>(
   ReaderIO<ENV, B> Function(A) f,
@@ -21,7 +21,7 @@ ReaderIO<ENV, B> Function<ENV>(ReaderIO<ENV, A>) map<A, B>(
 ReaderIO<ENV2, A> Function<A>(ReaderIO<ENV1, A>) local<ENV1, ENV2>(
   ENV1 Function(ENV2) f,
 ) =>
-    <A>(rio) => (env2) => rio(f(env2));
+    <A>(rio) => (env2) => () => rio(f(env2))();
 
 ReaderIO<ENV, A> asks<ENV, A>(
   A Function(ENV) f,
